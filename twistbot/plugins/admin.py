92fba196f225ca1486b3100f141b6eee4399845c
Andrew Brown <brownan@gmail.com>
2012-04-24 18:37:52 -0400

    added .voice and .devoice commands to IRCAdmin

--- twistbot/plugins/admin.py
+++ twistbot/plugins/admin.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import shlex
+from functools import wraps
 
 from twisted.internet import reactor
 from twisted.python import log
@@ -8,6 +9,23 @@ from twisted.internet import defer
 
 from ..command import CommandPluginSuperclass
 from ..transport import Event
+
+def check_pm(func):
+    """Checks if the PRIVMSG sent to `channel` is actually a PM, in which
+    case we should disregard it.
+
+    This decorator wraps command callbacks, and makes that check.
+
+    """
+    @wraps(func)
+    def newfunc(self, event, match):
+        mynick = self.pluginboss.loaded_plugins['irc.IRCBotPlugin'].client.nickname
+        if mynick == event.channel:
+            event.reply("Hey, you can't do that in here!")
+        else:
+            return func(self, event, match)
+    return newfunc
+
 
 class IRCAdmin(CommandPluginSuperclass):
     """A plugin to do various IRC OP-related tasks. The plugin does not keep
@@ -27,12 +45,29 @@ class IRCAdmin(CommandPluginSuperclass):
                 "'.KICK <nick> [reason]' Kicks a user from the current channel")
         self.define_command(".KICK")
 
+        self.install_command("(voice|VOICE) (?P<nick>[^ ]+)$",
+                "irc.op.voice",
+                self.voice)
+        self.help_msg("voice|VOICE",
+                "irc.op.voice",
+                "'.VOICE <nick>' Grants a user voice in the current channel",
+                )
+
+        self.install_command("(devoice|DEVOICE) (?P<nick>[^ ]+)$",
+                "irc.op.voice",
+                self.devoice)
+        self.help_msg("devoice|DEVOICE",
+                "irc.op.voice",
+                "'.DEVOICE <nick>' Revokes a user's voice in the current channel",
+                )
+
         self.install_command(r"op with external command (?P<command>.*)$",
                 "irc.op.configure",
                 self.set_op_external_command)
         self.install_command(r"op with chanserv$",
                 "irc.op.configure",
                 self.set_op_with_chanserv)
+
 
         # Maps channel names to a set of deferred objects indicating someone is
         # waiting for op on a channel
@@ -67,6 +102,7 @@ class IRCAdmin(CommandPluginSuperclass):
             log.msg("Relinquishing op")
             self.transport.send_event(deop)
 
+    @check_pm
     def kick(self, event, match):
         """A user has issued the kick command. Our job here is to acquire OP
         for this channel and set a callback to issue a kick event
@@ -77,16 +113,55 @@ class IRCAdmin(CommandPluginSuperclass):
         reason = groupdict.get("reason", None)
         channel = event.channel
 
-        deferred = self._get_op(event.channel)
+        kickevent = Event("irc.do_kick", channel=channel,
+                user=nick, reason=reason)
+
+        self._send_event_as_op(channel, kickevent, event.reply)
+
+    @check_pm
+    def voice(self, event, match):
+        groupdict = match.groupdict()
+        nick = groupdict['nick']
+        channel = event.channel
+
+        voiceevent = Event("irc.do_mode",
+                chan=channel,
+                set=True,
+                modes="v",
+                user=nick,
+                )
+        log.msg("Voicing %s in %s" % (nick, channel))
+        self._send_event_as_op(channel, voiceevent, event.reply)
+    @check_pm
+    def devoice(self, event, match):
+        groupdict = match.groupdict()
+        nick = groupdict['nick']
+        channel = event.channel
+
+        voiceevent = Event("irc.do_mode",
+                chan=channel,
+                set=False,
+                modes="v",
+                user=nick,
+                )
+        log.msg("De-voicing %s in %s" % (nick, channel))
+        self._send_event_as_op(channel, voiceevent, event.reply)
+
+    def _send_event_as_op(self, channel, event, reply):
+        """Gets op on the specified channel, then sends the given event
+        
+        The given reply callable is used if we need to reply to the channel (on
+        failure)
+        
+        """
+        deferred = self._get_op(channel)
         def fail(reason):
-            log.msg("Kick failed because we failed to get OP")
-            event.reply(reason.getErrorMessage())
+            log.err("failed because we failed to get OP")
+            reply(reason.getErrorMessage())
         def success(_):
             # Issue a kick request!
-            log.msg("OP succeeded, proceeding to issue a kick")
-            kickevent = Event("irc.do_kick", channel=channel,
-                    user=nick, reason=reason)
-            self.transport.send_event(kickevent)
+            log.msg("OP succeeded, proceeding to issue event %s" % event.__dict__)
+            self.transport.send_event(event)
         deferred.addCallbacks(success, fail)
 
     def set_op_external_command(self, event, match):
@@ -125,11 +200,15 @@ class IRCAdmin(CommandPluginSuperclass):
             mynick = self.pluginboss.loaded_plugins['irc.IRCBotPlugin'].client.nickname
 
             cmd = self.config['opcommand']
-            cmd = cmd.replace("%c", channel)
-            cmd = cmd.replace("%n", mynick)
-            cmd_parts = shlex.split(cmd)
-            log.msg("Executing %s" % cmd_parts)
-            proc_defer = getProcessOutput(cmd_parts[0], cmd_parts[1:], errortoo=True)
+            # Break the command up into parts. This is done before the %c and
+            # %n replacement to avoid any injection attacks
+            cmd_parts = shlex.split(cmd.encode("UTF-8"))
+            # Now do the replacements
+            cmd_parts = [x.replace("%c", channel.encode("UTF-8")) for x in cmd_parts]
+            cmd_parts = [x.replace("%n", mynick.encode("UTF-8"))  for x in cmd_parts]
+            log.msg("Executing %s" % cmd)
+            log.msg("Final arguments are %s" % cmd_parts)
+            proc_defer = getProcessOutput("/usr/bin/env", cmd_parts, errortoo=True)
             def finished(output):
                 if output.strip():
                     log.msg("Process finished. Returned %s" % output)
@@ -152,4 +231,4 @@ class IRCAdmin(CommandPluginSuperclass):
         """
         deferreds = self.waiting_for_op.pop(channel, set())
         for deferred in deferreds:
-            deferred.errback("OP request timed out. Check your config.")
+            deferred.errback("OP request timed out. Check your config and the error log.")
