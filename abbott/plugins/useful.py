599b66b6b223fe311fb935d8f50ef93ee345cf53
Andrew Brown <brownan@gmail.com>
2012-06-21 11:11:55 -0400

    added automatic url shortener plugin

--- abbott/plugins/useful.py
+++ abbott/plugins/useful.py
@@ -96,7 +96,7 @@ class Units(CommandPluginSuperclass):
                 cmdname="convert",
                 cmdmatch="convert|units?",
                 cmdusage="<from unit> [to <to unit>]",
-                argmatch="(?P<from>.+?)(?: to (?P<to>.+))?$",
+                argmatch="(?P<from>.+?)(?: (in|to) (?P<to>.+))?$",
                 permission=None,
                 helptext="Invokes the 'units' command to do a unit conversion.",
                 callback=self.invoke_units,
@@ -177,3 +177,51 @@ class Mueval(CommandPluginSuperclass):
             if len(line) >= maxlen:
                 line = line[:maxlen-3] + "..."
             event.reply(line)
+
+class URLShortener(BotPlugin):
+    minlength=80
+    urlmatcher = re.compile(r"""
+        \b
+        (                       # Capture 1: entire matched URL
+          (?:
+            https?://               # http or https protocol
+            |                       #   or
+            www\d{0,3}[.]           # "www.", "www1.", "www2." … "www999."
+            |                           #   or
+            [a-z0-9.\-]+[.][a-z]{2,4}/  # looks like domain name followed by a slash
+          )
+          (?:                       # One or more:
+            [^\s()<>]+                  # Run of non-space, non-()<>
+            |                           #   or
+            \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
+          )+
+          (?:                       # End with:
+            \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
+            |                               #   or
+            [^\s`!()\[\]{};:'".,<>?«»“”‘’]        # not a space or one of these punct chars
+          )
+        )
+        """,
+        re.VERBOSE | re.IGNORECASE)
+
+    def start(self):
+        self.listen_for_event("irc.on_privmsg")
+
+        import googl
+        self.shortener = googl.Googl()
+
+    def on_event_irc_on_privmsg(self, event):
+
+        match = self.urlmatcher.search(event.message)
+
+        if not match: return
+
+        url = match.group(1)
+
+        if len(url) < self.minlength:
+            return
+        
+        log.msg("Shortening '%s'" % url)
+        shortened = self.shortener.shorten(url)
+
+        event.reply("^ %s" % shortened['id'])
