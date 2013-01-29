08e248a69fc4182ccb860dc70ea9ca07ff0dd229
Andrew Brown <brownan@gmail.com>
2013-01-29 10:36:11 -0500

    added a repr plugin
    to help see what various control sequences look like to the bot

--- abbott/plugins/logger.py
+++ abbott/plugins/logger.py
@@ -1,6 +1,7 @@
 import pprint
 
 from ..pluginbase import BotPlugin
+from ..command import CommandPluginSuperclass
 
 class Log(BotPlugin):
     def start(self):
@@ -10,3 +11,16 @@ class Log(BotPlugin):
         print
         print "Received event %s" % (event.eventtype,)
         print pprint.pformat(event.__dict__)
+
+class Repr(CommandPluginSuperclass):
+    def start(self):
+        super(Repr, self).start()
+        self.install_command(
+                cmdname="repr",
+                callback=(lambda event,match:
+                        event.reply(repr(match.groupdict()['text']))
+                        ),
+                argmatch="(?P<text>.*)$",
+                cmdusage="<text>",
+                helptext="echos the repr of your text",
+                )
